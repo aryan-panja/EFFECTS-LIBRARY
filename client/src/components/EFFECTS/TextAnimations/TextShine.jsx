acf8920528eff033affb68ffd6cf069d685aefc8
Aryan Panja <apanja_be22@thapar.edu>
2024-11-12 18:00:04 +0530

    done aaj ka

--- client/src/components/EFFECTS/TextAnimations/TextShine.jsx
+++ client/src/components/EFFECTS/TextAnimations/TextShine.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const TextShine = ({children, className, ...props}) => {
     return (
-        <p className={cn(`font-bold py-3 text-center text-4xl font-hack bg-gradient-to-r from-black via-white to-black bg-clip-text text-transparent bg-[length:80%] animate-shine tracking-widest duration-[100s]`, className)} {...props}>
+        <p className={cn(`font-bold py-3 text-center text-4xl bg-gradient-to-r from-black via-white to-black bg-clip-text text-transparent bg-[length:80%] animate-shine tracking-widest`, className)} {...props}>
             {children}
         </p>
     )
